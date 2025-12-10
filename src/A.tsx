fa39fb1111d121ca27ae8bb616910478429876ae
axtk <axtkx@inbox.ru>
2025-12-10 22:55:11 +0300

    rephrase jsx output of route links

--- src/A.tsx
+++ src/A.tsx
@@ -1,8 +1,8 @@
 import type { AProps } from "./types/AProps.ts";
 import { useLinkClick } from "./useLinkClick.ts";
 
-export const A = (props: AProps) => {
+export const A = ({ children, ...props }: AProps) => {
   let handleClick = useLinkClick(props);
 
-  return <a {...props} href={String(props.href)} onClick={handleClick}/>;
+  return <a {...props} href={String(props.href)} onClick={handleClick}>{children}</a>;
 };
--- src/Area.tsx
+++ src/Area.tsx
@@ -1,8 +1,8 @@
 import type { AreaProps } from "./types/AreaProps.ts";
 import { useLinkClick } from "./useLinkClick.ts";
 
-export const Area = (props: AreaProps) => {
+export const Area = ({ alt, ...props }: AreaProps) => {
   let handleClick = useLinkClick(props);
 
-  return <area {...props} href={String(props.href)} onClick={handleClick}/>;
+  return <area {...props} href={String(props.href)} onClick={handleClick} alt={alt}/>;
 };
