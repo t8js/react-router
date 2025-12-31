81b4dfdcc02bb85b56a016d18b828f901af362ec
axtk <axtkx@inbox.ru>
2025-12-31 16:45:19 +0300

    use default behavior for non-self-target links

--- src/useLinkClick.ts
+++ src/useLinkClick.ts
@@ -8,18 +8,18 @@ import { RouteContext } from "./RouteContext.ts";
 import type { AProps } from "./types/AProps.ts";
 import type { AreaProps } from "./types/AreaProps.ts";
 
-export function useLinkClick({ onClick }: AProps | AreaProps) {
+export function useLinkClick({ target, onClick }: AProps | AreaProps) {
   let route = useContext(RouteContext);
 
   return useCallback(
     (event: ReactMouseEvent<HTMLAnchorElement & HTMLAreaElement>) => {
       onClick?.(event);
 
-      if (!event.defaultPrevented && isRouteEvent(event)) {
+      if (!event.defaultPrevented && isRouteEvent(event) && (!target || target === "_self")) {
         event.preventDefault();
         route.navigate(getNavigationOptions(event.currentTarget));
       }
     },
-    [route, onClick],
+    [route, target, onClick],
   );
 }
